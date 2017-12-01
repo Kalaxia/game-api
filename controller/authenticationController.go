8ffa25480acf37542dd9c40944436ed7c7f38eb8
Axel Venet <kern046@gmail.com>
2017-12-01 12:56:45 +0100

    Implement JSON WebToken generation and sending to portal

--- controller/authenticationController.go
+++ controller/authenticationController.go
@@ -3,12 +3,18 @@ package controller
 import (
   "fmt"
   "net/http"
-  //"encoding/json"
+  "encoding/json"
 	"io"
 	"io/ioutil"
-  //"kalaxia-game-api/security"
+  "kalaxia-game-api/security"
+  "github.com/dgrijalva/jwt-go"
 )
 
+/**
+ * This method receives crypted data from the portal, containing user credentials
+ * If the user is not registered, we create a player account.
+ * In any case, a JWT is returned, used by the player to authenticate
+ */
 func AuthenticatePlayer(w http.ResponseWriter, r *http.Request) {
   var body []byte
   var err error
@@ -18,9 +24,23 @@ func AuthenticatePlayer(w http.ResponseWriter, r *http.Request) {
 	if err = r.Body.Close(); err != nil {
     panic(err)
   }
+  jsonData := security.Decrypt(body)
+  var data map[string]interface{}
+  if err = json.Unmarshal(jsonData, &data); err != nil {
+    panic(err)
+  }
+  token := getNewJWT(data)
+  //json.NewEncoder(w).Encode(JwtToken{Token: tokenString})
+  w.Write(security.Encrypt([]byte(token)))
+}
 
-  fmt.Println(body);
-
-  //decryptedData := "test"
-
+func getNewJWT(data map[string]interface{}) string {
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+        "username": data["username"],
+    })
+    tokenString, error := token.SignedString([]byte("secret"))
+    if error != nil {
+        fmt.Println(error)
+    }
+    return tokenString
 }
