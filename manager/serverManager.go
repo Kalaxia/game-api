402ab2363f478a97810d54a2f65476af2e0c9ce5
Axel Venet <kern046@gmail.com>
2017-12-08 13:17:39 +0100

    Implement server retrieval method by signature

--- manager/serverManager.go
+++ manager/serverManager.go
@@ -4,6 +4,14 @@ import(
   "kalaxia-game-api/model/server"
 )
 
+func GetServerBySignature(signature string) *model.Server {
+  server := model.Server{Signature: signature}
+  if err := db.Model(&server).Where("signature = ?", signature).Select(); err != nil {
+    return nil
+  }
+  return &server
+}
+
 func CreateServer(name string, serverType string, signature string) model.Server {
   server := model.Server{
     Name: name,
