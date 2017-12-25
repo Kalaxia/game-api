7ca0d3214ce1222bd2625200d7832d59cd8be76c
Axel Venet <kern046@gmail.com>
2017-12-25 03:44:34 +0100

    Fix unique constraints for player table

--- build/migrations/2_players.up.sql
+++ build/migrations/2_players.up.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS players(
   id SERIAL PRIMARY KEY,
-  username VARCHAR(180) NOT NULL UNIQUE,
-  pseudo VARCHAR(180) NOT NULL UNIQUE,
-  server_id int references servers(id),
+  username VARCHAR(180) NOT NULL,
+  pseudo VARCHAR(180) NOT NULL,
+  server_id int references servers(id) NOT NULL,
   created_at timestamptz NOT NULL,
-  updated_at timestamptz NOT NULL
+  updated_at timestamptz NOT NULL,
+  CONSTRAINT username_constraint UNIQUE (username, server_id),
+  CONSTRAINT pseudo_constraint UNIQUE (pseudo, server_id)
 );
