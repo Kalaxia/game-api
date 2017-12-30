387c7e568e63930f34d22fb1ed9bf527dc2ede31
Axel Venet <kern046@gmail.com>
2017-12-30 16:58:40 +0100

    Implement model for factions and update ids type

--- model/player/player.go
+++ model/player/player.go
@@ -7,10 +7,10 @@ import(
 
 type(
   Player struct {
-    Id int16 `json:"id"`
+    Id uint16 `json:"id"`
     Username string `json:"-" sql:"type:varchar(180);not null;unique"`
     Pseudo string `json:"pseudo" sql:"type:varchar(180);not null;unique"`
-    ServerId int16 `json:"-"`
+    ServerId uint16 `json:"-"`
     Server *model.Server `json:"-"`
     CreatedAt time.Time `json:"created_at"`
     UpdatedAt time.Time `json:"updated_at"`
--- model/server/server.go
+++ model/server/server.go
@@ -4,7 +4,7 @@ import "fmt"
 
 type(
   Server struct {
-    Id int16 `json:"id"`
+    Id uint16 `json:"id"`
     Name string `json:"name" sql:"type:varchar(100);not null;unique"`
     Type string `json:"type" sql:"type:varchar(20);not null"`
     Signature string `json:"_" sql:"type:varchar(125);not null;unique"`
