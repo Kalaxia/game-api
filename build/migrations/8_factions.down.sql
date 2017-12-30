ffb1688d8841a81116f794acf230c4b053aa1ec7
Axel Venet <kern046@gmail.com>
2017-12-30 16:59:05 +0100

    Implement migration script for factions table

---
+++ build/migrations/8_factions.down.sql
@@ -0,0 +1,1 @@
+DROP TABLE faction__factions;
---
+++ build/migrations/8_factions.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS faction__factions(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(60) NOT NULL,
+  description TEXT NOT NULL,
+  server_id int references servers(id)
+);
