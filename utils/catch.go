298702f93128c23d812e13b59bf9c202d08abbd4
Axel Venet <kern046@gmail.com>
2018-04-18 10:33:34 +0200

    Implement error handler and exception processors

---
+++ utils/catch.go
@@ -0,0 +1,72 @@
+package utils
+
+import(
+    "log"
+    "net/http"
+    "kalaxia-game-api/exception"
+)
+
+func ErrorHandler(next http.HandlerFunc) http.HandlerFunc {
+    return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+        defer CatchHttpException(w)
+        next(w, req)
+    })
+}
+
+
+func CatchHttpException(w http.ResponseWriter) {
+    r := recover()
+    if r == nil {
+        return
+    }
+    if exception, ok := r.(*exception.HttpException); ok {
+        message := ""
+        if exception.Exception.Error != nil {
+            message = "; [Error]: " + exception.Exception.Error.Error()
+        }
+        if exception.Exception.Message != "" || message != "" {
+            log.Println("[Exception]: " + exception.Exception.Message + message)
+        }
+        SendJsonResponse(w, exception.Code, exception)
+        return
+    }
+    if exception, ok := r.(*exception.Exception); ok {
+        message := ""
+        if exception.Error != nil {
+            message = "; [Error]: " + exception.Error.Error()
+        }
+        if exception.Message != "" || message != "" {
+            log.Println("[Exception]: " + exception.Message + message)
+        }
+        SendJsonResponse(w, 500, exception)
+        return
+    }
+    if err, ok := r.(error); ok {
+        log.Println("[Error]: " + err.Error())
+        SendJsonResponse(w, 500, "Internal server error")
+        return
+    }
+    panic(r)
+}
+
+func CatchException() {
+    r := recover()
+    if r == nil {
+        return
+    }
+    if exception, ok := r.(*exception.Exception); ok {
+        message := ""
+        if exception.Error != nil {
+            message = "; [Error]: " + exception.Error.Error()
+        }
+        if exception.Message != "" || message != "" {
+            log.Println("[Exception]: " + exception.Message + message)
+        }
+        return
+    }
+    if err, ok := r.(error); ok {
+        log.Println("[Error]: " + err.Error())
+        return
+    }
+    panic(r)
+}
