10220e5b1817eb1453d01385eaf12373aadc140d
Axel Venet <kern046@gmail.com>
2019-04-27 14:29:38 +0200

    Implement entity and migration scripts for combats

---
+++ build/migrations/29_fleet_combats.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS fleet__combats(
+  id SERIAL PRIMARY KEY,
+  attacker_id int NOT NULL references fleet__fleets(id) ON DELETE CASCADE,
+  defender_id int NOT NULL references fleet__fleets(id) ON DELETE CASCADE,
+  attacker_ships json not null,
+  defender_ships json not null,
+  attacker_losses json not null,
+  defender_losses json not null,
+  is_victory boolean not null,
+  begin_at timestamptz not null,
+  end_at timestamptz
+);
