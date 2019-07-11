641e48b9585b4efb2c4ce085247c7e771a4ab140
Axel Venet <kern046@gmail.com>
2019-07-11 18:01:41 +0200

    Improve error handling and error logs

--- api/utils_catch.go
+++ api/utils_catch.go
@@ -3,6 +3,8 @@ package api
 import(
     "log"
     "net/http"
+    "runtime"
+    "strconv"
 )
 
 func ErrorHandler(next http.HandlerFunc) http.HandlerFunc {
@@ -13,68 +15,59 @@ func ErrorHandler(next http.HandlerFunc) http.HandlerFunc {
 }
 
 func CatchHttpException(w http.ResponseWriter) {
-    r := recover()
-    if r == nil {
-        return
+    if isError, code, message := CatchException(); isError {
+        SendJsonResponse(w, code, &HttpException{
+            Exception: Exception{ Message: message },
+            Code: code,
+        })
     }
-    if exception, ok := r.(*HttpException); ok {
-        message := ""
-        if exception.Exception.Error != nil {
-            message = "; [Error]: " + exception.Exception.Error.Error()
-        }
-        if exception.Exception.Message != "" || message != "" {
-            log.Println("[Exception]: " + exception.Exception.Message + message)
-        }
-        SendJsonResponse(w, exception.Code, exception)
-        return
-    }
-    if exception, ok := r.(*Exception); ok {
-        message := ""
-        if exception.Error != nil {
-            message = "; [Error]: " + exception.Error.Error()
-        }
-        if exception.Message != "" || message != "" {
-            log.Println("[Exception]: " + exception.Message + message)
-        }
-        SendJsonResponse(w, 500, exception)
-        return
-    }
-    if err, ok := r.(error); ok {
-        log.Println("[Error]: " + err.Error())
-        SendJsonResponse(w, 500, "Internal server error")
-        return
-    }
-    panic(r)
 }
 
-func CatchException() {
+func CatchException() (bool, int, string) {
     r := recover()
     if r == nil {
-        return
+        return false, 200, ""
     }
+    finalMessage := "Internal server error"
+    code := 500
+
     if exception, ok := r.(*HttpException); ok {
         message := ""
         if exception.Exception.Error != nil {
             message = "; [Error]: " + exception.Exception.Error.Error()
         }
         if exception.Exception.Message != "" || message != "" {
-            log.Println("[Exception]: " + exception.Exception.Message + message)
+            log.Println("[Exception]: ", exception.Exception.Message + message)
         }
-        return
-    }
-    if exception, ok := r.(*Exception); ok {
+        code = exception.Code
+        finalMessage = exception.Exception.Message
+    } else if exception, ok := r.(*Exception); ok {
         message := ""
         if exception.Error != nil {
             message = "; [Error]: " + exception.Error.Error()
         }
         if exception.Message != "" || message != "" {
-            log.Println("[Exception]: " + exception.Message + message)
+            log.Println("[Exception]: ", exception.Message + message)
         }
-        return
-    }
-    if err, ok := r.(error); ok {
+    } else if err, ok := r.(error); ok {
         log.Println("[Error]: " + err.Error())
-        return
     }
-    panic(r)
+    pc := make([]uintptr, 10)
+    if n := runtime.Callers(0, pc); n > 0 {
+        logFrames(pc, n)
+    }
+    return true, code, finalMessage
+}
+
+func logFrames(pc []uintptr, n int) {
+    pc = pc[:n]
+    frames := runtime.CallersFrames(pc)
+
+    for {
+        frame, more := frames.Next()
+        log.Println(frame.Function + " - " + frame.File + ":l" + strconv.Itoa(frame.Line))
+        if !more {
+            break
+        }
+    }
 }
