d581a222212aade5a57709bf5a2bc0e974cc2b9e
Axel Venet <kern046@gmail.com>
2019-08-12 17:05:38 +0200

    Fix construction state trigger and functions removal

--- build/migrations/35_points_production.up.sql
+++ build/migrations/35_points_production.up.sql
@@ -9,9 +9,9 @@ UPDATE map__planet_buildings SET status = 'operational' WHERE construction_state
 ALTER TABLE map__planet_buildings DROP construction_state_id, ADD construction_state_id INT DEFAULT NULL REFERENCES map__planet_point_productions;
 ALTER TABLE ship__ships DROP construction_state_id, ADD construction_state_id INT DEFAULT NULL REFERENCES map__planet_point_productions;
 
+DROP TRIGGER trigger_ship_construction_state_removal ON ship__ships;
+DROP TRIGGER trigger_building_construction_state_removal ON map__planet_buildings;
+DROP FUNCTION remove_ship_construction_state();
+DROP FUNCTION remove_building_construction_state();
 DROP TABLE map__planet_construction_states;
 DROP TABLE ship__construction_states;
-DROP FUNCTION remove_ship_construction_state;
-DROP FUNCTION remove_building_construction_state;
-DROP TRIGGER trigger_ship_construction_state_removal;
-DROP TRIGGER trigger_building_construction_state_removal;
