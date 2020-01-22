a8f40617714113e3c032b7576313b3cb81c5788e
Axel Venet <kern046@gmail.com>
2020-01-22 08:38:09 +0100

    Fix faction settings test

--- api/faction_settings_test.go
+++ api/faction_settings_test.go
@@ -15,10 +15,13 @@ func TestCreateFactionSettings(t *testing.T) {
 	if len(factions[0].Settings) == 0 {
 		t.Errorf("Faction should have 5 settings, got %d", len(factions[0].Settings))
 	}
-	if factions[1].Settings[0].IsPublic != true {
-		t.Errorf("First setting should be public")
+	for _, s := range factions[1].Settings {
+		if s.Name == FactionSettingsRegime && s.IsPublic != true {
+			t.Errorf("First setting should be public")
+		}
+		if s.Name == FactionSettingsMotionDuration && s.Value != 6 {
+			t.Errorf("Motion duration setting value should equal 6, got %d", s.Value)
+		}
 	}
-	if factions[1].Settings[1].Value != 6 {
-		t.Errorf("Motion duration setting value should equal 6, got %d", factions[1].Settings[1].Value)
-	}
+	
 } 
